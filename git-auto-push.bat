d1e323d6171cd1181aaeb4015916b720abee6b58
Copy <1617110693@qq.com>
2026-05-05 15:31:25 +0800

    updated

--- git-auto-push.bat
+++ git-auto-push.bat
@@ -1,27 +1,16 @@
-@echo off
-chcp 65001 > nul  :: 解决中文乱码
-setlocal enabledelayedexpansion
-
 :: ===================== 【文件夹路径】=====================
-cd /d "D:\notes"  :: 改成你的路径，比如你的MarkdownNote.md所在文件夹
+cd /d "D:\notes" 
 :: =====================================================================
 
-:: 1. 用wmic获取固定格式的时间
-for /f "skip=1 tokens=1-6 delims=.+- " %%a in ('wmic os get localdatetime') do (
-    if "%%a" NEQ "" (
-        set "datetime=%%a"
-        set "datetime=!datetime:~0,12!"  :: 截取前12位：年4+月2+日2+时2+分2
-    )
-)
 
-:: 2. 执行 Git 操作
+::  执行 Git 操作
 echo.
 echo ===================== Implement Git Commit =====================
-echo Commit Messages: updated!datetime!
+echo Commit Messages: updated
 echo --------------------------------------------------------
 
 git add .
-git commit -m "updated!datetime!"
+git commit -m "updated"
 git push
 
 echo.
